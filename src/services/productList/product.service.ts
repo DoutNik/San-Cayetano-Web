bf97a17229740cd54b68deee9e31682d6e5a9481
Carlos Emanuel Klema <carlosklema@hotmail.com>
2024-05-02 21:37:25 -0300

    Styles

--- src/services/productList/product.service.ts
+++ src/services/productList/product.service.ts
@@ -8,9 +8,9 @@ export class ProductService {
   getProducts(): Product[] {
     // Retorna la lista de productos
     return [
-      new Product(1, 'Muzzarela', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, oregano y aceitunas', ''),
-      new Product(1, 'Calabresa', 'Pizza', 'mediana', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, salame y aceitunas', ''),
-      new Product(1, 'Fugazzeta', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, cebolla, oregano y aceitunas', ''),
+      new Product(1, 'Muzzarela', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, oregano y aceitunas', 'cayetano'),
+      new Product(1, 'Calabresa', 'Pizza', 'mediana', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, salame y aceitunas', 'cayetano'),
+      new Product(1, 'Fugazzeta', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, cebolla, oregano y aceitunas', 'cayetano'),
       new Product(1, 'Pollo', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, pollo, oregano y aceitunas', ''),
       new Product(1, 'Especial', 'Pizza', 'mediana', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, jamon cocido y aceitunas', ''),
       new Product(1, 'Napolitana', 'Pizza', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, tomate en rodajas, oregano y aceitunas', ''),
