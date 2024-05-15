7d6d8a734cefef208fec8c4f981987c0bc9967eb
Carlos Emanuel Klema <carlosklema@hotmail.com>
2024-05-14 22:36:03 -0300

    recategorizacion de productos

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,18 +1,31 @@
+export enum ProductSize {
+  small = 'chica',
+  large = 'grande',
+}
+
+export enum ProductType {
+  pizza = 'Pizza',
+  sinAlcohol = 'Bebida sin alcohol',
+  conAlcohol = 'Bebida con alcohol',
+  empanada = 'Empanada',
+  calzone = 'Calzone',
+  lomo = 'Lomo',
+}
+
 export class Product {
-    id: number;
-    name: string;
-    category: string;
-    description: string;
-    size: string;
-    brand: string;
-  
-    constructor(id: number, name: string, category: string, size: string, description: string, brand: string) {
-      this.id = id;
-      this.name = name;
-      this.category = category;
-      this.size = size;
-      this.description = description;
-      this.brand = brand;
-    }
+  id: number;
+  name: string;
+  category: ProductType;
+  size: ProductSize;
+  description: string;
+  brand: string;
+
+  constructor(id: number, name: string, category: ProductType, size: ProductSize, description: string, brand: string) {
+    this.id = id;
+    this.name = name;
+    this.category = category;
+    this.size = size;
+    this.description = description;
+    this.brand = brand;
   }
-  
+}
--- src/services/productList/product.service.ts
+++ src/services/productList/product.service.ts
@@ -1,50 +1,278 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../../models/product.model';
+import { Product, ProductSize, ProductType } from '../../models/product.model';
+import { Category } from '../../models/category.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  getProducts(): Product[] {
-    // Retorna la lista de productos
+  getCategories(): Category[] {
     return [
-      // ________ID,___nombre,____categoria,_tamaño,_descripcion
-      //PIZZAS
-      new Product(1, 'Muzzarela', 'Pizzas', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, oregano y aceitunas', 'cayetano'),
-      new Product(2, 'Calabresa', 'Pizzas', 'mediana', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, salame y aceitunas', 'cayetano'),
-      new Product(3, 'Fugazzeta', 'Pizzas', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, cebolla, oregano y aceitunas', 'cayetano'),
-      new Product(4, 'Pollo', 'Pizzas', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, pollo, oregano y aceitunas', ''),
-      new Product(5, 'Especial', 'Pizzas', 'mediana', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, jamon cocido y aceitunas', ''),
-      new Product(6, 'Napolitana', 'Pizzas', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, tomate en rodajas, oregano y aceitunas', ''),
-      new Product(7, 'Champignones', 'Pizzas', 'Grande', 'Pizza con masa cacera, salsa de tomate, queso muzzarela, champignones, oregano y aceitunas', ''),
-      //BEBIDAS
-      new Product(9, 'Coca-Cola', 'Bebidas', 'Grande', '', ''),
-      new Product(10, 'Coca-Cola Zero', 'Bebidas', 'Grande', '', ''),
-      new Product(11, 'Fanta', 'Bebidas', 'Grande', '', ''),
-      new Product(12, 'Fanta Zero', 'Bebidas', 'Grande', '', ''),
-      new Product(13, 'Sprite', 'Bebidas', 'Grande', '', ''),
-      new Product(14, 'Sprite Zero', 'Bebidas', 'Grande', '', ''),
-      new Product(15, 'Aquarius Pera', 'Bebidas', 'Grande', '', ''),
-      new Product(16, 'Aquarius Pomelo', 'Bebidas', 'Grande', '', ''),
-      new Product(17, 'Aquarius Manzana', 'Bebidas', 'Grande', '', ''),
-      new Product(18, 'Aquarius Naranja', 'Bebidas', 'Grande', '', ''),
-      new Product(19, 'Coca-Cola', 'Bebidas', 'chica', '', ''),
-      new Product(20, 'Coca-Cola Zero', 'Bebidas', 'chica', '', ''),
-      new Product(21, 'Fanta', 'Bebidas', 'chica', '', ''),
-      new Product(22, 'Fanta Zero', 'Bebidas', 'chica', '', ''),
-      new Product(23, 'Sprite', 'Bebidas', 'chica', '', ''),
-      new Product(24, 'Sprite Zero', 'Bebidas', 'chica', '', ''),
-      new Product(25, 'Aquarius Pera', 'Bebidas', 'chica', '', ''),
-      new Product(26, 'Aquarius Pomelo', 'Bebidas', 'chica', '', ''),
-      new Product(27, 'Aquarius Manzana', 'Bebidas', 'chica', '', ''),
-      new Product(28, 'Aquarius Naranja', 'Bebidas', 'chica', '', ''),
+      new Category('Pizzas', [
+        new Product(
+          1,
+          'Muzzarela',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, oregano y aceitunas',
+          'cayetano'
+        ),
+        new Product(
+          2,
+          'Calabresa',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, salame y aceitunas',
+          'cayetano'
+        ),
+        new Product(
+          3,
+          'Fugazzeta',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, cebolla, oregano y aceitunas',
+          'cayetano'
+        ),
+        new Product(
+          4,
+          'Pollo',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, pollo, oregano y aceitunas',
+          ''
+        ),
+        new Product(
+          5,
+          'Especial',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, jamon cocido y aceitunas',
+          ''
+        ),
+        new Product(
+          6,
+          'Napolitana',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, tomate en rodajas, oregano y aceitunas',
+          ''
+        ),
+        new Product(
+          7,
+          'Champignones',
+          ProductType.pizza,
+          ProductSize.large,
+          'Pizza con masa cacera, salsa de tomate, queso muzzarela, champignones, oregano y aceitunas',
+          ''
+        ),
+      ]),
 
-      new Product(29, 'Imperial Golden', 'Bebidas', 'grande', 'Cerveza rubia suave', ''),
-      new Product(30, 'Imperial Stout', 'Bebidas', 'grande', 'Cerveza negra dulce', ''),
-      new Product(31, 'Imperial Clasica', 'Bebidas', 'grande', 'Cerveza rubia clasica', ''),
-      new Product(32, 'Heineken', 'Bebidas', 'grande', 'Cerveza rubia clasica', ''),
-      new Product(33, 'Vino', 'Bebidas', 'Grande', 'Vino blanco dulce', ''),
-
+      new Category('Bebidas sin alcohol', [
+        new Product(
+          9,
+          'Coca-Cola',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          10,
+          'Coca-Cola Zero',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          11,
+          'Fanta',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          12,
+          'Fanta Zero',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          13,
+          'Sprite',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          14,
+          'Sprite Zero',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          15,
+          'Aquarius Pera',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          16,
+          'Aquarius Pomelo',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          17,
+          'Aquarius Manzana',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          18,
+          'Aquarius Naranja',
+          ProductType.sinAlcohol,
+          ProductSize.large,
+          '',
+          ''
+        ),
+        new Product(
+          19,
+          'Coca-Cola',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          20,
+          'Coca-Cola Zero',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          21,
+          'Fanta',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          22,
+          'Fanta Zero',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          23,
+          'Sprite',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          24,
+          'Sprite Zero',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          25,
+          'Aquarius Pera',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          26,
+          'Aquarius Pomelo',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          27,
+          'Aquarius Manzana',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+        new Product(
+          28,
+          'Aquarius Naranja',
+          ProductType.sinAlcohol,
+          ProductSize.small,
+          '',
+          ''
+        ),
+      ]),
+      new Category('Bebidas con alcohol', [
+        new Product(
+          29,
+          'Imperial Golden',
+          ProductType.conAlcohol,
+          ProductSize.large,
+          'Cerveza rubia suave',
+          ''
+        ),
+        new Product(
+          30,
+          'Imperial Stout',
+          ProductType.conAlcohol,
+          ProductSize.large,
+          'Cerveza negra dulce',
+          ''
+        ),
+        new Product(
+          31,
+          'Imperial Clasica',
+          ProductType.conAlcohol,
+          ProductSize.large,
+          'Cerveza rubia clasica',
+          ''
+        ),
+        new Product(
+          32,
+          'Heineken',
+          ProductType.conAlcohol,
+          ProductSize.large,
+          'Cerveza rubia clasica',
+          ''
+        ),
+        new Product(
+          33,
+          'Vino',
+          ProductType.conAlcohol,
+          ProductSize.large,
+          'Vino blanco dulce',
+          ''
+        ),
+        // Otras bebidas con alcohol...
+      ]),
+      // Otras categorías...
     ];
   }
 }
